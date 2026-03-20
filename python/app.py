4dc3e364032075017f71be36f4cf9fd1c6893357
Alfreditann <alfred.ulsberg@gmail.com>
2026-03-20 13:40:57 +0100

    chore: mer database

--- python/app.py
+++ python/app.py
@@ -3,8 +3,20 @@ import sqlite3
 conn = sqlite3.connect('users.db')
 
 conn.execute("""CREATE TABLE IF NOT EXISTS users(
-    id INT,
-    username TEXT
+    id INT PRIMARY KEY,
+    username TEXT NOT NULL,
+    name TEXT NOT NULL,
+    bruker_id INT NOT NULL 
+)""")
+conn.execute("""CREATE TABLE IF NOT EXISTS notes(
+    id INT PRIMARY KEY,
+    username TEXT NOT NULL,
+    name TEXT NOT NULL,
+    bruker_id INT NOT NULL,
+
+    FOREIGN KEY (bruker_id) REFERENCES users (bruker_id) ON DELETE CASCADE ON UPDATE CASCADE
+
+
 )""")
 
 conn.commit()
